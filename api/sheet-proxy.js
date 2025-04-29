f875e6141d8bbed18b2dea21585ff38fe3dde0f9
hpajic <105848923+hpajic@users.noreply.github.com>
2025-04-29 14:11:35 +0200

    Add files via upload

--- api/sheet-proxy.js
+++ api/sheet-proxy.js
@@ -1,8 +1,8 @@
 export default async function handler(req, res) {
-    const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbwQLgjZQfdX9juNavg7zanh6735nS67Lv8OoCY-IMMAdbFxz4V72HC8fnBMq8p1gMNH/exec";
+  const SHEET_API_URL = "https://script.google.com/macros/s/AKfycbwQLgjZQfdX9juNavg7zanh6735nS67Lv8OoCY-IMMAdbFxz4V72HC8fnBMq8p1gMNH/exec";
 
-  if (req.method === "POST" && req.body.action === "delete") {
-    const response = await fetch(SHEET_API_URL + "?action=delete", {
+  if (req.method === "POST") {
+    const response = await fetch(SHEET_API_URL, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(req.body),
@@ -17,22 +17,15 @@ export default async function handler(req, res) {
     const data = await response.text();
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.status(200).send(data);
-  } else if (req.method === "POST") {
-    const response = await fetch(SHEET_API_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(req.body),
-    });
-    const data = await response.text();
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(200).send(data);
-  } else if (req.method === "OPTIONS") {
+    return;
+  }
+  if (req.method === "OPTIONS") {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
     res.setHeader("Access-Control-Allow-Headers", "Content-Type");
     res.status(200).end();
-  } else {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.status(405).send("Method Not Allowed");
+    return;
   }
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.status(405).send("Method Not Allowed");
 }
